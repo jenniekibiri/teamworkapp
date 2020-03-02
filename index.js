e6c9b912ac8bf8661b8df55be67b15cf86688534
jenniekibiri <josphatje@gmail.com>
2020-03-02 19:05:44 +0300

    create a specFile

---
+++ index.js
@@ -0,0 +1,8 @@
+const express =require('express');
+const  app =express();
+app.get('/',(req,res)=>{
+    res.send("hello server")
+
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>console.log(`server is running at port ${PORT}`));
